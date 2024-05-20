8f71f1c810596fc67e226d101995926b26563357
3Triple-c <152387685+3Triple-c@users.noreply.github.com>
2024-05-20 23:03:13 +0100

    Add files via upload

---
+++ triple_c.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// const areYouPlayingBanjo = function (name) {
+//   const fame = name;
+//   const letters = [...name];
+//   const [firstLetter, ...otherLetters] = letters;
+// //   console.log(firstLetter);
+// //   console.log(otherLetters);
+
+//   if (firstLetter === "r" || firstLetter === "R") {
+//     return `${name} plays banjo`;
+//   } else {
+//     return `${name} does not play banjo`;
+//   }
+// };
+// console.log(areYouPlayingBanjo('ROMMY'));
+
+
+
+
+const time = document.querySelector('.time')
+// time.textContent =
